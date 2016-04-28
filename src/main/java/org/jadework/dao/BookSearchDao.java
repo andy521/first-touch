101e5393e12a730ae58c07db70f585d3019d1c5b
Jade ZHANG <jadezhang@JadedeMacBook-Pro.local>
2016-04-28 15:58:43 +0800

    2016年04月28日15:58:41

---
+++ src/main/java/org/jadework/dao/BookSearchDao.java
@@ -0,0 +1,16 @@
+package org.jadework.dao;
+
+/**
+ * write Title here
+ * <p>
+ * write Description here
+ * </p>
+ *
+ * @author Jade ZHANG E-mail:pioneer_zcy@163.com
+ * @copyright 2015-2016 sh.lianjia.com.
+ * @date 16/4/19
+ */
+
+public class BookSearchDao {
+
+}
